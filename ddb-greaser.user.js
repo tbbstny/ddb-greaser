0771f2a67c557a53ba7007fd69e5fbd68528f9e6
tbbstny <tbbstny@users.noreply.github.com>
2022-11-06 18:12:47 -0600

    Add MathJS to evaluate Player Defense

--- ddb-greaser.user.js
+++ ddb-greaser.user.js
@@ -9,23 +9,28 @@
 // @downloadURL	 https://github.com/tbbstny/ddb-greaser/raw/main/tmkwtd.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @require      https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/mathjs/11.3.2/math.min.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_addStyle
 // ==/UserScript==
+console.info('D&D Beyond Greaser Starting');
 
 const $ = window.jQuery;
 
+/* eslint-disable no-multi-spaces */
 const THE_MONSTERS_KNOW = "the_monsters_know";
+const PLAYER_DEFENSE    = "player_defense";
 const PARENTHETICALS    = /\(.*\)/;
 
 const MONSTER_CONTAINER = `.encounter-details-monster-summary-info-panel,.encounter-details__content-section--monster-stat-block,.combat-tracker-page__content-section--monster-stat-block,.monster-details-modal__body`;
 const MONSTER_NAME      = `.mon-stat-block__name`;
+/* eslint-enable */
 
 // Style for "The Monsters Know What They're Doing" search icon
 GM_addStyle(`.${THE_MONSTERS_KNOW} {padding: 0 0 0 5px}`);
 GM_addStyle(`.icon {color: #8F4A5F; background: #E2C0C6; font-size: 30px; width: 30px; height: 30px; padding: 0; border-radius: 50%; text-align: center; line-height: 30px; vertical-align: -25%;`);
-
+GM_addStyle(`.${PLAYER_DEFENSE} {color: #8F4A5F; background: #E2C0C6; border-radius: 5%;`);
 
 (function() {
     'use strict';
@@ -34,7 +39,7 @@ GM_addStyle(`.icon {color: #8F4A5F; background: #E2C0C6; font-size: 30px; width:
     const documentModified = function(mutations, observer) {
         const monster = $(MONSTER_CONTAINER);
         const monster_name = monster.find(MONSTER_NAME).text();
-        console.log("Doc modified, new monster : ", monster_name, " !=? ", last_monster_name);
+        console.debug("Doc modified, new monster : ", monster_name, " !=? ", last_monster_name);
         if (monster_name !== last_monster_name) {
             last_monster_name = monster_name;
             degrease();
@@ -45,29 +50,60 @@ GM_addStyle(`.icon {color: #8F4A5F; background: #E2C0C6; font-size: 30px; width:
     // Watch DOM for changes
     const observer = new window.MutationObserver(documentModified);
     observer.observe(document, { "subtree": true, "childList": true, attributes: true, });
-    console.log('D&D Beyond Greaser Loaded');
+    console.info('D&D Beyond Greaser Loaded');
 })();
 
 
 function degrease() {
     $(`.${THE_MONSTERS_KNOW}`).remove();
+    $(`.${PLAYER_DEFENSE}`).remove();
 }
 
 function grease() {
     addTMKSearchLink();
+    addPlayerDefence();
 }
 
 
 /**
- * Find Monster Name, and add link to TMKWTD search
+ * Grab the Monster's name, and add a link to search for the Monsters at TMKWTD blog.
  */
 function addTMKSearchLink() {
     $('a.mon-stat-block__name-link').each(function(index, link) {
         var $el = $(link);
         if(!$el.siblings().length) {
             var href = "https://www.themonstersknow.com/?s=" + $el.text().replace(PARENTHETICALS,'').trim().replaceAll(' ', '+');
-            console.log("Add TMK Search Link: " + href);
+            console.debug("Add TMK Search Link: " + href);
             $el.after(`<a class="${THE_MONSTERS_KNOW}" href="${href}" target="_blank"><iconify-icon icon="game-icons:behold" class="icon" title='Search "The Monsters Know What They&#39re Doing"'></iconify-icon></a>`);
         }
     });
 }
+
+/**
+ *
+ */
+function addPlayerDefence() {
+    $('p:contains(" to hit")').each(function(i, p) {
+        let match = $(p).text().match(/([+-][0-9]+) to hit/);
+        let toHitBonus = match[1]
+
+        $(p).contents().each((j, node) => {
+            var $node = $(node);
+            if(node.nodeType === 3 && / to hit/.test(node.nodeValue)) {
+                var playerDefense = math.evaluate(`${toHitBonus} + 12`);
+                $node[0].textContent=$node.text().replace(' to hit', ` to hit (${playerDefense} Player Defense)`);
+                // $( ` <span class="${PLAYER_DEFENSE}">(${playerDefense} Player Defense)</p>` ).appendTo($(p));
+            }
+        });
+    });
+}
+
+
+function addCharacterDefence() {
+    $('div.ddbc-armor-class-box__value').each(function(i, ac) {
+        $(ac).attr("title","Player Defense = d20 + (AC - 10)");
+        $(ac).text("???");
+    });
+}
+
+
